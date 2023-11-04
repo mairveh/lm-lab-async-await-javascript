7e46bf604229dbb7d4296510e95a43854cbef054
Merve <babutine@yahoo.com>
2023-11-04 14:38:14 +0000

    feat: consume fetch with async and await for Exercise 2

--- exercise002.js
+++ exercise002.js
@@ -2,11 +2,15 @@ import fetch from "node-fetch";
 
 const jsonTypicode = "https://jsonplaceholder.typicode.com/todos/1";
 
-const fetchData = (apiEndPoint) => {
-  fetch(apiEndPoint)
-    .then((response) => response.json())
-    .then((json) => console.log(json))
-    .catch((error) => console.log(error));
+const fetchData2 = async (apiEndPoint) => {
+  const response = await fetch(apiEndPoint)
+  const json = await response.json()
+  try {
+    console.log(json)
+  }
+  catch(err) {
+    console.log(err)
+  }
 };
 
-fetchData(jsonTypicode);
+fetchData2(jsonTypicode);
