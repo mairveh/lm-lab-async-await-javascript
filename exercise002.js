712c9f6d0275d9d9fdf9eb2a09e0bb8f86651d0e
Merve <babutine@yahoo.com>
2023-11-04 14:53:43 +0000

    feat: consume fetch with async and await with a different API endpoint, TheCocktailDb

--- exercise002.js
+++ exercise002.js
@@ -1,12 +1,13 @@
 import fetch from "node-fetch";
 
-const jsonTypicode = "https://jsonplaceholder.typicode.com/todos/1";
+const jsonTypicode = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=11145";
 
 const fetchData2 = async (apiEndPoint) => {
   const response = await fetch(apiEndPoint)
   const json = await response.json()
+  const instructions = await json.drinks[0].strInstructions
   try {
-    console.log(json)
+    console.log(`Old Fashioned instructions: ${instructions}`)
   }
   catch(err) {
     console.log(err)
