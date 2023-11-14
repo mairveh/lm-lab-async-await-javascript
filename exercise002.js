a6405998ae60a293b48be915b66dda786165497d
Merve <babutine@yahoo.com>
2023-11-14 16:51:40 +0000

    refactor: move all statements in try block to catch all errors

--- exercise002.js
+++ exercise002.js
@@ -3,10 +3,10 @@ import fetch from "node-fetch";
 const jsonTypicode = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=11145";
 
 const fetchData2 = async (apiEndPoint) => {
-  const response = await fetch(apiEndPoint)
-  const json = await response.json()
-  const instructions = await json.drinks[0].strInstructions
   try {
+    const response = await fetch(apiEndPoint)
+    const json = await response.json()
+    const instructions = await json.drinks[0].strInstructions
     console.log(`Old Fashioned instructions: ${instructions}`)
   }
   catch(err) {
